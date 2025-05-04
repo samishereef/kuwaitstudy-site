6755e9580560a2a66a98e1c33b052d6327df82cb
samishereef <samishereef@yahoo.com>
2025-05-04 22:47:34 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "100px" }}>
+      <h1>Welcome to KuwaitStudy</h1>
+      <p>This site is under development. Come back soon!</p>
     </div>
   );
 }
